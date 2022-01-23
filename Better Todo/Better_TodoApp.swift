9643655b69ff9d7e2e1269161dd041bdb9c40a76
Dominic Kolbe <kolbedominic@gmail.com>
2022-01-23 00:44:26 +0000

    init xcode project

---
+++ Better Todo/Better_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_TodoApp.swift
+//  Better Todo
+//
+//  Created by Dominic Kolbe on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Better_TodoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# BetterTodo
+<p align="center">
+  <h2 align="center">Better Todo</h2>
+  <p align="center">Create your todos. Quickly and easy.<p>
+</p>
+
+---
+
+## LICENSE
+
+Copyright © 2022 [Dominic Kolbe](https://dominickolbe.dk) :de:
